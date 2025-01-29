f9d31c40b630315ca8b326b29676b627dcb78bad
Geetansh Chahal <geetanshchahal949@gmail.com>
2025-01-30 01:12:29 +0530

    minor fixes

--- app/page.tsx
+++ app/page.tsx
@@ -18,14 +18,14 @@ const Home = () => {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        {/* <StepLoader /> */}
+        <StepLoader />
         {/* <FloatingNav navItems={navItems} /> */}
         <Hero />
         {/* <Grid /> */}
         <Clients />
         <Experience />
         <Blogs />
-        {/* <RecentProjects /> */}
+        <RecentProjects />
         {/* <RecentProjects /> */}
         {/* <Approach /> */}
         {/* Rate my Portfolio */}
