06c9782f073b29d1d01ff31261821afae7a4d267
Geetansh Chahal <geetanshchahal@Geetanshs-MacBook-Air.local>
2025-09-30 15:28:44 +0200

    minor fixes

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,11 @@
 "use client";
 
-import { loadingStates, navItems } from "@/data";
+import { navItems } from "@/data";
 
 import Hero from "@/components/Hero";
 import Grid from "@/components/Grid";
 import Footer from "@/components/Footer";
 import Clients from "@/components/Clients";
-import Approach from "@/components/Approach";
 import Experience from "@/components/Experience";
 import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
